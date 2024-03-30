8a8e453b5583217bc222c6354be2fef55c15f456
Fernanda-Dantas <fer.dantas07@gmail.com>
2024-03-30 18:49:17 -0300

    criei o dominjector

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -15,7 +15,6 @@ export abstract class View<T> {
 
     }
 
-    @logarTempoDeExecucao(true)
     public update(model: T): void {
         let template = this.template(model);
         this.elemento.innerHTML = template;
